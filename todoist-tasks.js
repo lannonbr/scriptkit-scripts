1c7207ecb700594d5a78746617d9c419f996cb1e
Benjamin Lannon <benjamin@lannonbr.com>
2021-04-25 00:33:29 -0400

    Add todoist script

---
+++ todoist-tasks.js
@@ -0,0 +1,35 @@
+// Menu: Todoist Tasks
+// Description: View this week's tasks from Todoist
+// Author: Benjamin Lannon
+// Twitter: @lannonbr
+
+/** @type typeof import('todoist') */
+const todoistPackage = await npm("todoist");
+
+/** @type typeof import('dayjs') */
+const dayjs = await npm("dayjs");
+
+// API Token can be found here: https://todoist.com/prefs/integrations
+const token = await env("TODOIST_TOKEN");
+
+const client = todoistPackage.v8(token);
+
+await client.sync();
+
+let items = client.items.get();
+
+items = items.filter((item) => {
+  let dueDate = dayjs(item.due.date);
+
+  return dueDate.diff(dayjs(), "day") <= 7;
+});
+
+await arg(
+  "View this week's tasks",
+  items.map((item) => {
+    return {
+      name: item.content,
+      description: `Due: ${item.due.date}`,
+    };
+  })
+);
